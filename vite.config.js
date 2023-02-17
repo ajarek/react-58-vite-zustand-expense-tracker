43af663f6ac5a63f72fa9d45cf412ed30465ad56
ajarek <61388692+ajarek@users.noreply.github.com>
2023-02-17 21:09:51 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   base:"/react-58-vite-zustand-expense-tracker/",
   plugins: [react()],
